6d2a1f45c081348169a128b754b81f7f51d1e0d9
baocin <steele.pedersen@gmail.com>
2016-02-01 20:23:39 -0500

    More bacon

--- app/src/main/java/com/github/baocin/inclass03_pizzastore/MainActivity.java
+++ app/src/main/java/com/github/baocin/inclass03_pizzastore/MainActivity.java
@@ -25,90 +25,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-//        Button addToppingButton = (Button) findViewById(R.id.addToppingButton);
-//        addToppingButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-
         setContentView(R.layout.activity_main);
 
-//        RelativeLayout rl = new RelativeLayout(this);
-//        rl.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-//        setContentView(rl);
-//
-//        ImageView pizzaView = new ImageView(this);
-//        pizzaView.setImageResource(R.drawable.pizza);
-//        pizzaView.setId(R.id.pizzaIcon);
-////        pizzaView.setLayoutParams();
-//        rl.addView(pizzaView);
-//        toppingTableRow.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Table OnClick", "Topping Clicked: " + v.toString());
-//            }
-//        });
-
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-        toppingsList.add("bacon");
-
-
-
-//        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(getBaseContext().LAYOUT_INFLATER_SERVICE);
-//        View v = vi.inflate(R.layout.activity_main, null);
 
         Button clearButton = (Button) findViewById(R.id.clearPizzaButton);
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                toppingsList.re
+//                toppingsList.clear();
                 toppingsList = new ArrayList<String>();
+                clearTable();
                 Toast.makeText(getBaseContext(), "Cleared ArrayList", Toast.LENGTH_SHORT).show();
+                refreshTable();
             }
         });
 
+        Button addToppingButton = (Button) findViewById(R.id.addToppingButton);
+        addToppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                toppingsList.add("bacon");
 
-        TableRow toppingTableRow = (TableRow) findViewById(R.id.toppingTable1);
-
-
-
-        //Go through array list and make a new imageview for each
-//        int id = 0;
-        int counter = 1;
-        for (String topping:toppingsList) {
-            if (counter >= 5){
-                toppingTableRow = (TableRow) findViewById(R.id.toppingTable2);
+                refreshTable();
             }
-            ImageView toppingImageView = new ImageView(this);
-            switch (topping){
-                case "bacon":
-                    toppingImageView.setImageResource(R.drawable.bacon);
-                    break;
+        });
 
-            }
-//            toppingImageView.setId(id);
-            toppingImageView.setAdjustViewBounds(true);
-            toppingImageView.setMaxHeight(10);
-
-            toppingImageView.setMaxWidth(10);
-
-            toppingTableRow.addView(toppingImageView);
-            counter++;
-        }
-
-
-//// insert into main view
-//        ViewGroup insertPoint = (ViewGroup) findViewById(R.id.insert_point);
-//        insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
 
 
 
@@ -118,5 +61,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void clearTable() {
+        TableRow toppingTableRow = (TableRow) findViewById(R.id.toppingTable1);
+        TableRow toppingTableRow2 = (TableRow) findViewById(R.id.toppingTable2);
 
+        toppingTableRow.removeAllViews();
+        toppingTableRow2.removeAllViews();
+    }
+
+    public void refreshTable(){
+        TableRow toppingTableRow = (TableRow) findViewById(R.id.toppingTable1);
+
+        int counter = 1;
+        for (String topping:toppingsList) {
+            if (counter > 5){
+                toppingTableRow = (TableRow) findViewById(R.id.toppingTable2);
+            }
+            ImageView toppingImageView = new ImageView(this);
+            switch (topping){
+                case "bacon":
+                    toppingImageView.setImageResource(R.drawable.bacon);
+                    break;
+
+            }
+
+            toppingImageView.setAdjustViewBounds(true);
+            toppingImageView.setMaxHeight(120);
+            toppingImageView.setMaxWidth(120);
+
+            toppingTableRow.addView(toppingImageView);
+            counter++;
+        }
+    }
 }
